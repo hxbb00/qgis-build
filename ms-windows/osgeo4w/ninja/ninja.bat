18f01e182268f42115b2bdd9afb0b450e9ba2bce
hxbb00 <hxbb00@qq.com>
2023-10-20 14:20:05 +0800

    Update ninja.bat

--- ms-windows/osgeo4w/ninja/ninja.bat
+++ ms-windows/osgeo4w/ninja/ninja.bat
@@ -1,7 +1,7 @@
 @echo off
 call %OSGEO4W_ROOT%\bin\o4w_env.bat
-call py3_env.bat
-call qt5_env.bat
+call %OSGEO4W_ROOT%\bin\py3_env.bat
+call %OSGEO4W_ROOT%\bin\qt5_env.bat
 call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 path %PATH%;c:\cygwin\bin;c:\program files\cmake\bin
 %OSGEO4W_ROOT%\bin\ninja -j4 -C ..\build-qgis-dev-x86_64
