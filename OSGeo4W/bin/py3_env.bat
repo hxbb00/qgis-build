55af447eef03e91c69c0cdc4e4fc6ec2bcfffb02
hxbb00 <hxbb00@qq.com>
2023-10-26 09:07:59 +0800

    Update py3_env.bat pip install qscintilla

--- OSGeo4W/bin/py3_env.bat
+++ OSGeo4W/bin/py3_env.bat
@@ -1,8 +1,9 @@
 SET PYTHONHOME=%OSGEO4W_ROOT%\apps\Python39
-SET PYTHONPATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages
+SET PYTHONPATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages;%PYTHONHOME%\Lib\site-packages\PyQt5
 SET PYTHONUTF8=1
 SET PYTHON_EXECUTABLE=%PYTHONHOME%\python.exe
 SET Python_ROOT_DIR=%PYTHONHOME%\
 PATH %PYTHONPATH%;%PATH%
 pip uninstall -y PyQt5
 pip install PyQt5 -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip install qscintilla -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
